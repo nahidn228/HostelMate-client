7bf1d079aa5c72378dacbc65973e0f7c358c0ee2
Nahid Hasan <nahidn228@gmail.com>
2025-01-14 21:01:39 +0600

    implement search sort filter functionality from server

--- src/pages/AllMeals.jsx
+++ src/pages/AllMeals.jsx
@@ -1,36 +1,53 @@
 /* eslint-disable no-unused-vars */
-import { useState } from "react";
-import InfiniteScroll from "react-infinite-scroller";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { toast } from "react-hot-toast";
 import MealsCard from "../components/MealsCard";
-import useMeals from "../hooks/useMeals";
 
 const AllMeals = () => {
-  const [meals] = useMeals();
-  const [getMeals, setGetMeals] = useState(meals);
+  // const [meals] = useMeals();
+  // const axiosPublic = useAxiosPublic();
+  const [jobs, setJobs] = useState([]);
+  const [filter, setFilter] = useState("");
+  const [search, setSearch] = useState("");
+  const [sort, setSort] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const [getMeals, setGetMeals] = useState([]);
   // console.log(meals);
   const [hasMore, setHasMore] = useState(true);
-  const fetchMoreData = () => {
-    setTimeout(() => {
-      setGetMeals(getMeals.concat(Array.from({ length: meals.length })));
-    }, 500);
-  };
+  useEffect(() => {
+    try {
+      const fetchAllData = async () => {
+        const { data } = await axios.get(
+          `http://localhost:5000/meals?filter=${filter}&search=${search}&sort=${sort}`
+        );
+        setGetMeals(data);
+      };
+      fetchAllData();
+    } catch (err) {
+      toast.error(err);
+    } finally {
+      setTimeout(() => setIsLoading(false), 2000);
+    }
+  }, [filter, search, sort]);
 
   return (
-    <div className="container lg:px-6 py-10 mx-auto min-h-[calc(100vh-306px)] flex flex-col justify-between">
+    <div className="container lg:px-6 py-10 mx-auto min-h-screen flex flex-col justify-between">
       <div>
         <div className="flex flex-col lg:flex-row justify-center items-center gap-5 ">
-         
-            <div className='flex flex-col md:flex-row gap-4'>
-              <select
-                name="category"
-                id="category"
-                className="border p-4 rounded-lg"
-              >
-                <option value="">Filter By Category</option>
-                <option value="Web Development">Breakfast</option>
-                <option value="Graphics Design">Lunch</option>
-                <option value="Digital Marketing"> Dinner</option>
-              </select>
+          <div className="flex flex-col md:flex-row gap-4">
+            <select
+              name="category"
+              id="category"
+              className="border p-4 rounded-lg"
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
+            >
+              <option value="">Filter By Category</option>
+              <option value="Breakfast">Breakfast</option>
+              <option value="Lunch">Lunch</option>
+              <option value="Dinner"> Dinner</option>
+            </select>
             <form>
               <div className="flex p-1 overflow-hidden border rounded-lg    focus-within:ring focus-within:ring-opacity-40 focus-within:border-blue-400 focus-within:ring-blue-300">
                 <input
@@ -39,6 +56,8 @@ const AllMeals = () => {
                   name="search"
                   placeholder="Enter Job Title"
                   aria-label="Enter Job Title"
+                  value={search}
+                  onChange={(e) => setSearch(e.target.value)}
                 />
 
                 <button className="px-1 md:px-4 py-3 text-sm font-medium tracking-wider text-gray-100 uppercase transition-colors duration-300 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:bg-gray-600 focus:outline-none">
@@ -46,14 +65,14 @@ const AllMeals = () => {
                 </button>
               </div>
             </form>
-            </div>
+          </div>
 
-        
           <div className="flex gap-4 items-center">
             <select
               name="category"
               id="category"
               className="border p-4 rounded-md"
+              onChange={(e) => setSort(e.target.value)}
             >
               <option value="">Sort By Price</option>
               <option value="dsc">Descending Order</option>
@@ -63,18 +82,21 @@ const AllMeals = () => {
           </div>
         </div>
         <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-          {meals.map((meal) => (
+          {getMeals.map((meal) => (
+            <MealsCard key={meal._id} meal={meal} />
+          ))}
+          {/* {getMeals.map((meal) => (
             <InfiniteScroll
               key={meal._id}
               dataLength={getMeals.length}
               pageStart={0}
               loadMore={meal}
-              next={fetchMoreData}
+              next={fetchAllData}
               hasMore={hasMore}
             >
               <MealsCard meal={meal} />
             </InfiniteScroll>
-          ))}
+          ))} */}
         </div>
       </div>
     </div>
