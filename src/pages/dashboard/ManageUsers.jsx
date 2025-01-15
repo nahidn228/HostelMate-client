9ac40a8ab38ede018f8f05a5c8057028988a9101
Nahid Hasan <nahidn228@gmail.com>
2025-01-15 23:05:55 +0600

    display all user in a table view

--- src/pages/dashboard/ManageUsers.jsx
+++ src/pages/dashboard/ManageUsers.jsx
@@ -14,6 +14,47 @@ const ManageUsers = () => {
   return (
     <div>
       <h2 className="text-2xl">All Users {users.length} </h2>
+
+      <div className="overflow-x-auto">
+        <table className="table">
+          {/* head */}
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Badge</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user, idx) => (
+              <tr key={user._id}>
+                <th>{idx + 1}</th>
+                <td>
+                  <div className="flex items-center gap-3">
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img src={user?.photo} alt={user?.name} />
+                      </div>
+                    </div>
+                    <div>
+                      <div className="font-bold">{user?.name}</div>
+                    </div>
+                  </div>
+                </td>
+                <td>{user?.email}</td>
+                <td>{user?.badge}</td>
+                <td className="">Role</td>
+                <th>
+                  <button className="btn btn-ghost btn-xs">details</button>
+                </th>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
