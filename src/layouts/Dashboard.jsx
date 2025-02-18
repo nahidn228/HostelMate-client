1c45f62494253b4f201084061475e9450f0034b4
Nahid Hasan <nahidn228@gmail.com>
2025-02-18 15:15:59 +0600

    add dashboard navbar

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,61 +1,67 @@
-import { FaHome, FaList, FaUsers } from "react-icons/fa";
-import { FaCartPlus } from "react-icons/fa6";
-import { GiHotMeal } from "react-icons/gi";
-import {
-  MdEmail,
-  MdFastfood,
-  MdOutlineRestaurantMenu,
-  MdRateReview,
-  MdRestaurant,
-  MdUpcoming,
-} from "react-icons/md";
-import { RiHome7Fill } from "react-icons/ri";
+import { BsCart2 } from "react-icons/bs";
+import { GiOpenedFoodCan } from "react-icons/gi";
+import { GrHistory } from "react-icons/gr";
+import { IoHomeOutline, IoRestaurantOutline } from "react-icons/io5";
+import { MdOutlineFastfood } from "react-icons/md";
+import { TbBowlSpoon, TbUsers } from "react-icons/tb";
+import { TfiEmail, TfiWrite } from "react-icons/tfi";
 import { NavLink, Outlet } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
+import DashboardNavbar from "../pages/dashboard/DashboardNavbar";
 
 const Dashboard = () => {
   const [isAdmin] = useAdmin();
 
   return (
     <div className="flex">
-      <div className="w-64 min-h-screen bg-cyan-200">
+      <div className="w-72 min-h-screen bg-[#142943] text-white">
+        <div className="flex gap-2 items-center justify-center p-2 mt-4">
+          {/* <HostelMateIcon /> */}
+          <img
+            src="https://img.icons8.com/?size=100&id=zpM2C9OxTBQZ&format=png&color=000000"
+            alt="HostelMateIcon"
+            className="w-14"
+          />
+          <span className="font-bold text-white text-lg md:text-xl">HostelMate</span>
+        </div>
+        <div className="divider divider-neutral"></div>
         <ul className="menu">
           {isAdmin ? (
             <>
               <li>
                 <NavLink to="/dashboard/myAdminProfile">
-                  <RiHome7Fill />
+                  <IoHomeOutline />
                   My Profile (Admin)
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/manage-users">
-                  <FaUsers /> Manage Users
+                  <TbUsers /> Manage Users
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/add-meal">
-                  <MdRestaurant /> Add Meal
+                  <IoRestaurantOutline /> Add Meal
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/allMeals">
-                  <GiHotMeal /> All Meals
+                  <MdOutlineFastfood /> All Meals
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/allReviews">
-                  <MdRateReview /> All Reviews
+                  <TfiWrite /> All Reviews
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/serveMeals">
-                  <MdFastfood /> Serve Meals
+                  <TbBowlSpoon /> Serve Meals
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/upcomingMeals">
-                  <MdUpcoming /> Upcoming Meals
+                  <GiOpenedFoodCan /> Upcoming Meals
                 </NavLink>
               </li>
             </>
@@ -63,50 +69,51 @@ const Dashboard = () => {
             <>
               <li>
                 <NavLink to="/dashboard/userProfile">
-                  <RiHome7Fill />
+                  <IoHomeOutline />
                   My Profile (user)
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/mealRequest">
-                  <FaCartPlus /> Requested Meals
+                  <BsCart2 /> Requested Meals
                 </NavLink>
               </li>
               <li>
                 <NavLink to="/dashboard/myReviews">
-                  <MdRateReview /> My Reviews
+                  <TfiWrite /> My Reviews
                 </NavLink>
               </li>
 
               <li>
                 <NavLink to="/dashboard/paymentHistory">
-                  <FaList /> Payment History
+                  <GrHistory /> Payment History
                 </NavLink>
               </li>
             </>
           )}
 
           {/* Shared NavLink */}
-          <div className="divider"></div>
+          <div className="divider divider-neutral"></div>
           <li>
             <NavLink to="/">
-              <FaHome /> Home
+              <IoHomeOutline /> Home Page
             </NavLink>
           </li>
           <li>
             <NavLink to="/meals">
-              <MdOutlineRestaurantMenu /> Meals
+              <IoRestaurantOutline /> All Meals
             </NavLink>
           </li>
           <li>
             <NavLink to="/order/salad">
-              <MdEmail /> Contact
+              <TfiEmail /> Contact
             </NavLink>
           </li>
         </ul>
       </div>
 
-      <div className="flex-1 p-10">
+      <div className="flex-1 ">
+        <DashboardNavbar />
         <Outlet></Outlet>
       </div>
     </div>
--- src/pages/dashboard/DashboardNavbar.jsx
+++ src/pages/dashboard/DashboardNavbar.jsx
@@ -1,66 +1,157 @@
-
+import { useContext } from "react";
+import { IoSettingsSharp } from "react-icons/io5";
+import { AuthContext } from "../../providers/AuthProvider";
 
 const DashboardNavbar = () => {
-    return (
-        <div className="navbar bg-base-100">
-  <div className="flex-1">
-    <a className="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div className="flex-none">
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
-        <div className="indicator">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-5 w-5"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor">
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-          </svg>
-          <span className="badge badge-sm indicator-item">8</span>
-        </div>
+  const { user } = useContext(AuthContext);
+  return (
+    <div className=" flex justify-between items-center px-3 py-4">
+      <div className="">
+        {/* search bar */}
+
+        <form className="flex mx-auto">
+          <label className="sr-only" htmlFor="voice-search">
+            Search
+          </label>
+          <div className="relative w-full">
+            <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+              <svg
+                viewBox="0 0 21 21"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                className="w-4 h-4 text-gray-500 dark:text-gray-400"
+              >
+                <path
+                  d="M11.15 5.6h.01m3.337 1.913h.01m-6.979 0h.01M5.541 11h.01M15 15h2.706a1.957 1.957 0 0 0 1.883-1.325A9 9 0 1 0 2.043 11.89 9.1 9.1 0 0 0 7.2 19.1a8.62 8.62 0 0 0 3.769.9A2.013 2.013 0 0 0 13 18v-.857A2.034 2.034 0 0 1 15 15Z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  stroke="currentColor"
+                ></path>
+              </svg>
+            </div>
+            <input
+              required=""
+              placeholder="Search..."
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+              id="voice-search"
+              type="text"
+            />
+            <button
+              className="absolute inset-y-0 end-0 flex items-center pe-3"
+              type="button"
+            >
+              <svg
+                viewBox="0 0 16 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+                aria-hidden="true"
+                className="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white"
+              >
+                <path
+                  d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z"
+                  strokeWidth="2"
+                  strokeLinejoin="round"
+                  strokeLinecap="round"
+                  stroke="currentColor"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <button
+            className="inline-flex items-center py-2.5 px-3 ms-2 text-sm font-medium text-white bg-blue-700 rounded-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+            type="submit"
+          >
+            <svg
+              viewBox="0 0 20 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+              aria-hidden="true"
+              className="w-4 h-4 me-2"
+            >
+              <path
+                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                strokeWidth="2"
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                stroke="currentColor"
+              ></path>
+            </svg>
+            Search
+          </button>
+        </form>
       </div>
-      <div
-        tabIndex={0}
-        className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow">
-        <div className="card-body">
-          <span className="text-lg font-bold">8 Items</span>
-          <span className="text-info">Subtotal: $999</span>
-          <div className="card-actions">
-            <button className="btn btn-primary btn-block">View cart</button>
+      <div className="flex justify-between items-center space-x-2">
+        <div className="dropdown dropdown-end">
+          <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+            <div className="indicator">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
+                />
+              </svg>
+              <span className="badge badge-sm indicator-item">8</span>
+            </div>
+          </div>
+          <div
+            tabIndex={0}
+            className="card card-compact dropdown-content bg-base-100 z-[1] mt-3 w-52 shadow"
+          >
+            <div className="card-body">
+              <span className="text-lg font-bold">8 Items</span>
+              <span className="text-info">Subtotal: $999</span>
+              <div className="card-actions">
+                <button className="btn btn-primary btn-block">View cart</button>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-    <div className="dropdown dropdown-end">
-      <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
-        <div className="w-10 rounded-full">
-          <img
-            alt="Tailwind CSS Navbar component"
-            src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+
+        <div className="hover:scale-110 transition-all ease-in-out cursor-pointer hover:shadow-xl delay-500 animate-spin delay-10000 duration-1000  border-4 shadow-inner  rounded-full grid place-items-center z-0 h-10 w-10 relative ">
+          <IoSettingsSharp />
+        </div>
+
+        <div className="dropdown dropdown-end">
+          <div
+            tabIndex={0}
+            role="button"
+            className="btn btn-ghost btn-circle avatar"
+          >
+            <div className="w-10 rounded-full">
+              <img alt="Tailwind CSS Navbar component" src={user?.photoURL} />
+            </div>
+          </div>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+          >
+            <li>
+              <a className="justify-between">
+                Profile
+                <span className="badge">New</span>
+              </a>
+            </li>
+            <li>
+              <a>Settings</a>
+            </li>
+            <li>
+              <a>Logout</a>
+            </li>
+          </ul>
         </div>
       </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-        <li>
-          <a className="justify-between">
-            Profile
-            <span className="badge">New</span>
-          </a>
-        </li>
-        <li><a>Settings</a></li>
-        <li><a>Logout</a></li>
-      </ul>
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default DashboardNavbar;
