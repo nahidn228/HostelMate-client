db188abca79fe9f83eb800f7d57c49976ddf2dc2
Nahid Hasan <nahidn228@gmail.com>
2025-04-09 21:04:38 +0600

    updated payment field

--- src/pages/dashboard/Payment/Payment.jsx
+++ src/pages/dashboard/Payment/Payment.jsx
@@ -7,13 +7,19 @@ import CheckoutForm from "./CheckoutForm";
 const stripePromise = loadStripe(import.meta.env.VITE_PG_API_KEY);
 const Payment = () => {
   return (
-    <div>
-      <div className=' m-4 p-6 border-2 border-[#142943] rounded-lg  '>
-        <Elements stripe={stripePromise}>
-          <CheckoutForm />
-        </Elements>
-      </div>
+    <div className="min-h-[calc(100vh-10rem)] flex items-center justify-center bg-white px-4">
+    <div className="w-full max-w-lg border border-[#142943] rounded-xl p-6">
+      <h2 className="text-xl font-semibold text-[#142943] mb-4 text-center">
+        Make Payment 
+      </h2>
+      <Elements stripe={stripePromise}>
+        <CheckoutForm />
+      </Elements>
+      <p className="text-sm text-gray-500 text-center mt-4">
+        Secured by Stripe
+      </p>
     </div>
+  </div>
   );
 };
 
